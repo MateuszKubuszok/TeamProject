8b9fe26edd58e5e630820fc0815447b1e94d94b8
Mateusz Kubuszok <mathew.key@gmail.com>
2012-03-22 04:53:23 +0100

    Prekompilacja CSS

--- config/application.rb
+++ config/application.rb
@@ -47,6 +47,7 @@ module TeamProject
 
     # Enable the asset pipeline
     config.assets.enabled = true
+    config.assets.precompile += ['*.css']
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
